3cd528cb4115d4572d40fc6edd5488513bea3a1c
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-05-03 01:39:24 +0530

    updated the main

--- Xylophone/main.dart
+++ Xylophone/main.dart
@@ -1,58 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(XylophoneApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.red,
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.orange, soundNumber: 2),
+                buildKey(color: Colors.yellow, soundNumber: 3),
+                buildKey(color: Colors.green, soundNumber: 4),
+                buildKey(color: Colors.teal, soundNumber: 5),
+                buildKey(color: Colors.blue, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
+              ],
           ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.orange,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.yellow,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.green,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.blue,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.indigo,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.purple,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
