b3ecb71adad57b1482b31688a2e767040580f031
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-09 21:49:27 +0530

    Added dart files

---
+++ Flash Chat⚡/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: "/welcome",
+      routes: {
+        ChatScreen.id: (context)=>ChatScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        RegistrationScreen.id:(context)=>RegistrationScreen(),
+        WelcomeScreen.id:(context)=>WelcomeScreen(),
+      }
+    );
+  }
+}
