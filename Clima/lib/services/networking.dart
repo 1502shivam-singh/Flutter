3411baaddd1ebd05d327f9a1ee3f1448c8f9c937
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-12 00:52:35 +0530

    Added networking and weather data files

---
+++ Clima/lib/services/networking.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+
+class Transferclass {
+  Future<String> getData(double lat, double long) async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=080e7086b058964bb0a435e677d9eef5&units=metric');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return data;
+    } else {
+      print(response.statusCode);
+      return null;
+    }
+  }
+
+  Future<String> getCityData(String city) async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=080e7086b058964bb0a435e677d9eef5&units=metric');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return data;
+    } else {
+      print(response.statusCode);
+      return null;
+    }
+  }
+}
